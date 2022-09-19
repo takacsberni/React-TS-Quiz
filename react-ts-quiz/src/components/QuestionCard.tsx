70e8bdd45a6896335e80aaac46f4b066f993c2d0
takacsberni <takacsberni@gmail.com>
2022-09-19 13:26:22 +0200

    feat: declare the type of props with it's properties

--- react-ts-quiz/src/components/QuestionCard.tsx
+++ react-ts-quiz/src/components/QuestionCard.tsx
@@ -1,3 +1,14 @@
 import React from "react";
-const QuestionCard = () => <div> Question Card </div>;
+
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNr: number;
+    totalQuestions: number;
+}
+const QuestionCard = () => (
+    <div> Question Card </div>
+)
     export default QuestionCard;
