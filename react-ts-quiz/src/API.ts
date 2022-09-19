88c1ac818ec093a832042b4ea83dc97cfd77edfd
takacsberni <takacsberni@gmail.com>
2022-09-19 14:44:41 +0200

    feat: map the incoming question and answers and return them

--- react-ts-quiz/src/API.ts
+++ react-ts-quiz/src/API.ts
@@ -1,8 +1,10 @@
+import {shuffleArray} from "./utils";
+
 export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 }
@@ -17,5 +19,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)=> {
     const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endPoint)).json();
-    console.log(data);
+    return data.results.map( (question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+        }
+    ))
 }
