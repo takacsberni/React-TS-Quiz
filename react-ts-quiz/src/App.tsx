3b376e7da5ef112abf2d663fb14401e6060214fb
takacsberni <takacsberni@gmail.com>
2022-09-19 14:09:27 +0200

    feat: set up some states

--- react-ts-quiz/src/App.tsx
+++ react-ts-quiz/src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionCard from "./components/QuestionCard";
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+
+    const startTrivia = async () => {};
+
     const startQuiz = async () => {
 
     }
