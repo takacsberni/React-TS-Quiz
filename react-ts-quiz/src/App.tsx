47dd76827fe1371d777e3bd7a880cfe4f4f23cc0
takacsberni <takacsberni@gmail.com>
2022-09-19 16:22:01 +0200

    feat: next question button shows only if we are not fetching, and we are in game mode, and if the user have given an answer and if we are not in the last question

--- react-ts-quiz/src/App.tsx
+++ react-ts-quiz/src/App.tsx
@@ -70,8 +70,10 @@ const App = () => {
         />
             ) : null}
 
-        {!gameOver && !loading && userAnswers.length === number+1}
+        {!gameOver && !loading && userAnswers.length === number+1 && userAnswers.length !== TOTAL_QUESTION-1 ? (
         <button className="next" onClick={nextQuestion}> Next one </button>
+            ): null
+        }
     </div>
   );
 }
